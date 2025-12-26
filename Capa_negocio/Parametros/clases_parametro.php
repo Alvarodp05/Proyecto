18aac3196c5c3f50a65b3e228877a8d73762a614
Alvarodp05 <alvarodelpino05@gmail.com>
2025-12-26 19:32:18 +0100

    Add files via upload

--- Capa_negocio/Parametros/clases_parametro.php
+++ Capa_negocio/Parametros/clases_parametro.php
@@ -1,83 +1,97 @@
 <?php
-// Ruta relativa desde Capa_negocio/Parametros/
-include_once (__DIR__ . "/../../Lib/BD/AccesoBD.php");
+// Proyecto/Capa_negocio/Parametros/clases_parametro.php
+require_once(__DIR__ . "/../../Lib/BD/AccesoBD.php");
 
-class Parametro {
+class Parametro
+{
+    public $id; 
+    public $id_parametro; // Identificador de texto
     public $id_maquina;
-    public $id;
-    public $label;
-    public $units;
-    
-    // Configuración
-    public $alarm_c_low;
-    public $alarm_c_high;
-    public $alarm_p_low;
-    public $alarm_p_high;
+    public $nombre;
+    public $unidades;
+    public $alarm_c_min;
+    public $alarm_c_max;
+    public $alarm_p_min;
+    public $alarm_p_max;
     public $rand_min;
     public $rand_max;
-
-    // Valores
     public $valor_actual;
     public $valor_ayer;
     public $valor_antier;
 
-    // Estado
-    public $alarma_activa = null; 
-
-    public function __construct($id_maquina, $id_parametro, $data) {
-        $this->id_maquina = $id_maquina;
-        $this->id = $id_parametro;
-        
-        $this->label = $data['label'];
-        $this->units = $data['units'];
-        $this->alarm_c_low = $data['alarm_c_low'];
-        $this->alarm_c_high = $data['alarm_c_high'];
-        $this->alarm_p_low = $data['alarm_p_low'];
-        $this->alarm_p_high = $data['alarm_p_high'];
-        $this->rand_min = $data['rand_min'];
-        $this->rand_max = $data['rand_max'];
-
-        $this->valor_actual = $data['valor_actual'];
-        $this->valor_ayer = $data['valor_ayer'];
-        $this->valor_antier = $data['valor_antier'];
-
-        $this->gestionarValores();
-        $this->comprobarAlarmas();
+    public static function obtenerPorMaquina($id_maquina) {
+        $bd = new AccesoBD();
+        $sql = "SELECT * FROM parametros WHERE id_maquina = :id";
+        $bd->prepararConsulta($sql);
+        $bd->lanzarConsultaPreparada([':id' => $id_maquina]);
+        $lista = $bd->registrosConsulta('Parametro');
+        $bd->desconectar();
+        return $lista;
     }
 
-    private function gestionarValores() {
-        $acabo_de_guardar = isset($_SESSION['flag_acabo_de_guardar']) && $_SESSION['flag_acabo_de_guardar'] === true;
-
-        if ($acabo_de_guardar && $this->valor_actual !== null) {
-            // Mantener valor
-        } else {
-            // Simular nuevos
-            $min = (int)$this->rand_min; 
-            $max = (int)$this->rand_max;
-            if ($max <= $min) $max = $min + 1;
-
-            $this->valor_actual = rand($min, $max);
-            $this->valor_ayer = rand($min, $max);
-            $this->valor_antier = rand($min, $max);
-
-            $bd = new AccesoBD();
-            $bd->actualizarValor('parametros', $this->id_maquina, $this->id, 'valor_actual', $this->valor_actual);
-            $bd->actualizarValor('parametros', $this->id_maquina, $this->id, 'valor_ayer', $this->valor_ayer);
-            $bd->actualizarValor('parametros', $this->id_maquina, $this->id, 'valor_antier', $this->valor_antier);
+    public static function actualizarManual($postParams) {
+        if (!is_array($postParams)) return;
+        
+        $bd = new AccesoBD();
+        $sql = "UPDATE parametros SET valor_actual = :val WHERE id_parametro = :id";
+        $bd->prepararConsulta($sql);
+        
+        foreach ($postParams as $id_txt => $valor) {
+            $valor_limpio = str_replace(',', '.', $valor);
+            if ($valor_limpio !== '' && is_numeric($valor_limpio)) {
+                $bd->lanzarConsultaPreparada([':val' => $valor_limpio, ':id' => $id_txt]);
+            }
         }
+        $bd->desconectar();
     }
 
-    private function comprobarAlarmas() {
-        $val = floatval($this->valor_actual);
+    public static function simular($parametros) {
+        $bd = new AccesoBD();
+        $sql = "UPDATE parametros SET valor_actual = :val WHERE id_parametro = :id";
+        $bd->prepararConsulta($sql);
         
-        if ($val < $this->alarm_c_low || $val > $this->alarm_c_high) {
-            $this->alarma_activa = 'correctiva';
-        } elseif (($val > $this->alarm_c_low && $val < $this->alarm_p_low) || 
-                  ($val < $this->alarm_c_high && $val > $this->alarm_p_high)) {
-            $this->alarma_activa = 'preventiva';
-        } else {
-            $this->alarma_activa = null;
+        foreach ($parametros as $p) {
+            $p = (object)$p;
+            $min = (float)($p->rand_min ?? 0);
+            $max = (float)($p->rand_max ?? 100);
+            if ($min >= $max) $max = $min + 0.1;
+            $aleatorio = mt_rand((int)($min*100), (int)($max*100)) / 100;
+            
+            if (isset($p->id_parametro)) {
+                $bd->lanzarConsultaPreparada([':val' => $aleatorio, ':id' => $p->id_parametro]);
+            }
         }
+        $bd->desconectar();
+    }
+
+    // ESTA FUNCION ES NECESARIA PARA EL BORRADO
+    public static function borrarPorMaquina($id_maquina) {
+        $bd = new AccesoBD();
+        $bd->prepararConsulta("DELETE FROM parametros WHERE id_maquina = :id");
+        $bd->lanzarConsultaPreparada([':id' => $id_maquina]);
+        $bd->desconectar();
+    }
+    
+    public static function guardarDesdeSesion($id_maquina, $datosParametros) {
+        $bd = new AccesoBD();
+        $bd->prepararConsulta("DELETE FROM parametros WHERE id_maquina = :id");
+        $bd->lanzarConsultaPreparada([':id' => $id_maquina]);
+
+        if (!empty($datosParametros)) {
+            $sqlInsP = "INSERT INTO parametros (id_parametro, id_maquina, nombre, unidades, alarm_c_min, alarm_c_max, alarm_p_min, alarm_p_max, rand_min, rand_max, valor_actual) 
+                        VALUES (:idp, :idm, :lbl, :uni, :cmin, :cmax, :pmin, :pmax, :rmin, :rmax, :valini)";
+            $bd->prepararConsulta($sqlInsP);
+            foreach ($datosParametros as $key => $p) {
+                $id_final = !empty($key) ? $key : uniqid();
+                $val_ini = isset($p['rand_min']) ? $p['rand_min'] : 0;
+                $bd->lanzarConsultaPreparada([
+                    ':idp' => $id_final, ':idm' => $id_maquina, ':lbl' => $p['label'], ':uni' => $p['units'] ?? '',
+                    ':cmin' => $p['alarm_c_low'], ':cmax' => $p['alarm_c_high'], ':pmin' => $p['alarm_p_low'], ':pmax' => $p['alarm_p_high'],
+                    ':rmin' => $p['rand_min'] ?? 0, ':rmax' => $p['rand_max'] ?? 100, ':valini' => $val_ini
+                ]);
+            }
+        }
+        $bd->desconectar();
     }
 }
 ?>
